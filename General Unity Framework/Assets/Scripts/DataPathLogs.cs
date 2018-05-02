7eaa6b774b34444b99c0ec0f6c3cad979aeeb1df
fuemocheng <fuemocheng@163.com>
2018-05-02 10:27:42 +0800

    Logs Viewer
    Logs Viewer

---
+++ General Unity Framework/Assets/Scripts/DataPathLogs.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataPathLogs : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        Debug.Log("Application.dataPath:" + Application.dataPath);
+        Debug.Log("Application.streamingAssetsPath:" + Application.streamingAssetsPath);
+        Debug.Log("Application.persistentDataPath:" + Application.persistentDataPath);
+        Debug.Log("Application.temporaryCachePath:" + Application.temporaryCachePath);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ General Unity Framework/README.md
@@ -0,0 +1,8 @@
+# General Unity Framewrok
+## 1、Log Viewer
+Unity-Logs-Viewer
+
+Reporter->Create
+## 2、Path Logs
+
+## 3、
