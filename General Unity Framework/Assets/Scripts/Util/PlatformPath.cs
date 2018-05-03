fdf471938ea26800a3f6b782f7c6c5d5acc656e8
fuemocheng <fuemocheng@163.com>
2018-05-03 10:56:47 +0800

    change PlatformPath
    change PlatformPath

--- General Unity Framework/Assets/Scripts/Util/PlatformPath.cs
+++ General Unity Framework/Assets/Scripts/Util/PlatformPath.cs
@@ -3,36 +3,37 @@ using System.Collections.Generic;
 using UnityEngine;
 
 /// <summary>
+/// 以下为绝对路径 Absolute Path
 ///
 /// Platform:   UNITY_EDITOR_WIN
 /// Application.dataPath:               F:/ProjectName/Assets
 /// Application.streamingAssetsPath:    F:/ProjectName/Assets/StreamingAssets
-/// Application.persistentDataPath:     C:/Users/username/AppData/LocalLow/company name/product name
-/// Application.temporaryCachePath:     C:/Users/username/AppData/Local/Temp/company name/product name
+/// Application.persistentDataPath:     C:/Users/username/AppData/LocalLow/CompanyName/ProductName
+/// Application.temporaryCachePath:     C:/Users/username/AppData/Local/Temp/CompanyName/ProductName
 /// 
-/// Platform:   STANDALONE_WIN          F:/Export/GameRoot/AppName.exe
+/// Platform:   STANDALONE_WIN          F:/Export/GameRoot/AppName.exe      //应用所在路径
 /// Application.dataPath:               F:/Export/GameRoot/AppName_Data
 /// Application.streamingAssetsPath:    F:/Export/GameRoot/AppName_Data/StreamingAssets
-/// Application.persistentDataPath:     C:/Users/username/AppData/LocalLow/company name/product name
-/// Application.temporaryCachePath:     C:/Users/username/AppData/Local/Temp/company name/product name
+/// Application.persistentDataPath:     C:/Users/username/AppData/LocalLow/CompanyName/ProductName
+/// Application.temporaryCachePath:     C:/Users/username/AppData/Local/Temp/CompanyName/ProductName
 /// 
 /// Platform:   UNITY_ANDROID
-/// Application.dataPath:               /data/app/package name-1.apk
-/// Application.streamingAssetsPath:    jar:file:///data/app/package name-1.apk!/assets
-/// Application.persistentDataPath:     /storage/sdcard/Android/data/package name/files
-/// Application.temporaryCachePath:     /storage/sdcard/Android/data/package name/cache
+/// Application.dataPath:               /data/app/com.Company.ProductName-1.apk
+/// Application.streamingAssetsPath:    jar:file:///data/app/com.Company.ProductName-1.apk!/assets
+/// Application.persistentDataPath:     /storage/sdcard/Android/data/com.Company.ProductName/files
+/// Application.temporaryCachePath:     /storage/sdcard/Android/data/com.Company.ProductName/cache
 ///
 /// Platform:   UNITY_EDITOR_OSX
-/// Application.dataPath:               /data/app/package name-1.apk
-/// Application.streamingAssetsPath:    jar:file:///data/app/package name-1.apk!/assets
-/// Application.persistentDataPath:     /storage/sdcard/Android/data/package name/files
-/// Application.temporaryCachePath:     /storage/sdcard/Android/data/package name/cache
+/// Application.dataPath:               /Users/username/Documents/ProjectName/Assets
+/// Application.streamingAssetsPath:    /Users/username/Documents/ProjectName/Assets/StreamingAssets
+/// Application.persistentDataPath:     /Users/username/Library/Application Support/CompanyName/ProductName
+/// Application.temporaryCachePath:     /var/folders/xx/xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx/T/CompanyName/ProductName
 ///
 /// Platform:   UNITY_IOS
-/// Application.dataPath:               /data/app/package name-1.apk
-/// Application.streamingAssetsPath:    jar:file:///data/app/package name-1.apk!/assets
-/// Application.persistentDataPath:     /storage/sdcard/Android/data/package name/files
-/// Application.temporaryCachePath:     /storage/sdcard/Android/data/package name/cache
+/// Application.dataPath:               /var/containers/Bundle/Application/xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx/ProductName.app/Data
+/// Application.streamingAssetsPath:    /var/containers/Bundle/Application/xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx/ProductName.app/Data/Raw
+/// Application.persistentDataPath:     /var/containers/Bundle/Application/xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx/Documents
+/// Application.temporaryCachePath:     /var/containers/Bundle/Application/xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx/Library/Caches
 /// 
 /// </summary>
 public class PlatformPath : MonoBehaviour
