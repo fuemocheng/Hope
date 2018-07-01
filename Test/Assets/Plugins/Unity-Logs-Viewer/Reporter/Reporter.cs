e1dcdbb0215c2e5681edb3a8a75ae16ab136469d
fuemocheng <fuemocheng@163.com>
2018-07-01 15:55:12 +0800

    Open project with Unity2018.1.5f1(64bit)
    Open project with Unity2018.1.5f1(64bit)
    fix error
    Assets/Plugins/Unity-Logs-Viewer/Reporter/Reporter.cs(2013,47): error CS0619: `UnityEngine.RuntimePlatform.OSXWebPlayer' is obsolete: `WebPlayer export is no longer supported in Unity 5.4+.'

--- Test/Assets/Plugins/Unity-Logs-Viewer/Reporter/Reporter.cs
+++ Test/Assets/Plugins/Unity-Logs-Viewer/Reporter/Reporter.cs
@@ -2010,10 +2010,11 @@ public class Reporter : MonoBehaviour
 			url = System.IO.Path.Combine(streamingAssetsPath, prefFile);
 		}
 
+        /*
 		if (Application.platform != RuntimePlatform.OSXWebPlayer && Application.platform != RuntimePlatform.WindowsWebPlayer)
 			if (!url.Contains("://"))
 				url = "file://" + url;
-
+        */
 
 		// float startTime = Time.realtimeSinceStartup;
 		WWW www = new WWW(url);
